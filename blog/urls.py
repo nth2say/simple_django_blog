2aa0c8cf0ac44fa43167f73d422bbcff99548779
udf404 <ripple21@gmail.com>
2015-07-20 17:26:58 +0800

    add detail page

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^list/$', views.ArticleListView.as_view(), name='article-list'),
+    url(r'^list/$', views.ArticleListView.as_view(), name='article_list'),
+    url(r'^ar/(?P<pk>[0-9]+)/$', views.ArticleDetailView.as_view(), name='article_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.utils import timezone
 
 from .models import Article
@@ -18,6 +19,19 @@ class ArticleListView(ListView):
         return context
 
 
+class ArticleDetailView(DetailView):
+    queryset = Article.objects.all()
+
+    def get_object(self):
+        # Call the superclass
+        object = super(ArticleDetailView, self).get_object()
+        # Return the object
+        return object
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
 
 def index(request):
     return HttpResponse("this is index")
